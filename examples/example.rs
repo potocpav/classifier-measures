3003f0f6a171833a75d17c2c835e7c32437c962c
Pavel Potocek <pavelpotocek@gmail.com>
2017-09-22 14:20:47 +0200

    Put example into a single file

--- examples/example.rs
+++ examples/example.rs
@@ -1,10 +1,41 @@
 
 extern crate classifier_measures;
 
-mod data;
-
-use data::{Sex, get_people};
 use classifier_measures::roc_auc;
+
+
+#[derive(PartialEq)]
+enum Sex { M, F }
+
+struct Person {
+    name: &'static str,
+    sex: Sex,
+}
+
+const FEMALES: [&str; 50] =
+    ["Kyra", "Kaylie", "Angelique", "Mckenzie", "Genevieve", "Liliana", "Kamila", "Kylee", "Amani",
+    "Liana", "Jaycee", "Athena", "Crystal", "Sage", "Francesca", "Lindsey", "Makena", "Amina",
+    "Sofia", "Hana", "Giselle", "Marley", "Shelby", "Lucy", "Giuliana", "Liberty", "Nataly",
+    "Marlie", "Leticia", "Belen", "Miley", "Ariella", "Evelin", "Saniya", "Erin", "Priscilla",
+    "Elisa", "Destinee", "Nathalia", "Julianna", "Isabella", "Paloma", "Christina", "Jaylene",
+    "Essence", "Zoe", "Cristina", "Shannon", "Aurora", "Alana"];
+
+const MALES: [&str; 50] =
+    ["Winston", "Issac", "James", "Braylen", "Reid", "Omar", "Dallas", "Anthony", "Jayce", "Bryce",
+    "Dane", "Yusuf", "Desmond", "Tony", "Kade", "Elvis", "Ronnie", "Alessandro", "Kadyn", "Jaydan",
+    "Alexander", "Brandon", "Paxton", "Shaun", "Ali", "Grayson", "Francis", "Jamie", "Luca",
+    "Izaiah", "Keshawn", "Terrell", "Ricky", "Jermaine", "Donovan", "John", "Frankie", "Thaddeus",
+    "Nathen", "King", "Aiden", "Joel", "Marcel", "Prince", "Aryan", "Jasiah", "Phoenix", "Mathew",
+    "Isaac", "Kane"];
+
+fn get_people() -> Vec<Person> {
+    FEMALES.into_iter()
+        .map(|name| Person { name, sex: Sex::F })
+        .chain(MALES.into_iter()
+        .map(|name| Person { name, sex: Sex::M }))
+        .collect::<Vec<_>>()
+}
+
 
 fn main() {
     let data = get_people();
